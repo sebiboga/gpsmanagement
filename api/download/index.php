0a2eb10a67ec02aa5c85e6f7cc36f43d96fcdbf9
sebastianboga <sebastian.boga@endava.com>
2020-02-23 12:40:22 +0200

    adding header and footer in KML

--- api/download/index.php
+++ api/download/index.php
@@ -1,17 +1,48 @@
 <?php
+
+$header_kml ='
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2">
+<Document>
+  <!-- Begin Style Definitions -->
+  <Style id="line1">
+    <LineStyle>
+      <color>FF007DEF</color>
+      <width>1</width>
+    </LineStyle>
+  </Style>
+  <Folder>
+    <name>Line Features</name>
+    <description>Line Features</description>
+    <Placemark>
+      <description>Unknown Line Type</description>
+      <styleUrl>#line1</styleUrl>
+      <LineString>
+        <coordinates>
+';
+
+$footer_kml = '
+ </coordinates>
+      </LineString>
+    </Placemark>
+  </Folder>
+</Document>
+</kml>
+';
 if(isset($_POST["kml"]))  
  {  
       require_once('../dbconnect.php'); 
       header('Content-Type: text/csv; charset=utf-8');  
       header('Content-Disposition: attachment; filename=traseu.kml');  
       $output = fopen("php://output", "w");  
-//      fputcsv($output, array( 'lat', 'lng'));  
+     fputcsv($output, $header_kml);  
       $query = "SELECT lat,lng FROM viatransilvanica";  
       $result = mysqli_query($con, $query);  
       while($row = mysqli_fetch_assoc($result))  
       {  
            fputcsv($output, $row);  
-      }  
+      }
+   fputcsv($output, $footer_kml); 	  
       fclose($output);  
  }  
  
